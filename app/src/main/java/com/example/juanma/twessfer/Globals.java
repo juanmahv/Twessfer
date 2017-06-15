14d98824788959f59fa1e6929c54ca9e1c83a710
Juanma de Hoyos <juanmahv@gmail.com>
2017-06-15 20:42:02 +0200

    Added Activity to display status details

---
+++ app/src/main/java/com/example/juanma/twessfer/Globals.java
@@ -0,0 +1,7 @@
+package com.example.juanma.twessfer;
+
+/**
+ * Created by juanma on 15.06.17.
+ */
+public class Globals {
+}
--- app/src/main/java/com/example/juanma/twessfer/Tweet.java
+++ app/src/main/java/com/example/juanma/twessfer/Tweet.java
@@ -2,7 +2,7 @@ package com.example.juanma.twessfer;
 
 import android.graphics.Bitmap;
 
-public class Row {
+public class Tweet {
     public twitter4j.Status status;
     public Bitmap avatar;
 }
